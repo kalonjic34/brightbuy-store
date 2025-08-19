fc42fd5b43c864d351fde9ac4da9b1485d8896ba
Christian <kalonjic34@gmail.com>
2025-08-19 14:22:06 +0200

    Implement product info view with 404 handling

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from . models import Category, Product
+from django.shortcuts import get_object_or_404
 
 def store(request):
     
@@ -16,3 +17,10 @@ def categories(request):
     
     return{'all_categories':all_categories}
 
+def product_info(request,slug):
+    
+    product = get_object_or_404(Product, slug= slug)
+    
+    context = {'product': product}
+    
+    return render(request, 'store/product-info.html', context)
