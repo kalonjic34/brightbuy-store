84cdc1786766dd57826f3ab24e8e36f8481da41c
Christian <kalonjic34@gmail.com>
2025-09-01 15:01:12 +0200

    Ensure email field is required in CreateUserForm

--- ecommerce/account/forms.py
+++ ecommerce/account/forms.py
@@ -10,12 +10,16 @@ class CreateUserForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(CreateUserForm, self).__init__(*args, **kwargs)
         
+        self.fields['email'].required = True
+        
     def clean_email(self):
         email = self.cleaned_data.get('email')
         
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError('This email is invalid')
         
-        if len(email >= 350):
+        if len(email) >= 350:
             
             raise forms.ValidationError('Your email is too long')
+        
+        return email
