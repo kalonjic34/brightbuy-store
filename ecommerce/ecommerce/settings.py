86ee92e20ed19c2bbb064e3f400b3cc10395c880
Christian <kalonjic34@gmail.com>
2025-09-01 15:01:47 +0200

    Implement Crispy Forms with Bootstrap4

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -42,8 +42,11 @@ INSTALLED_APPS = [
     'cart',
     'account',
     'mathfilters',
+    'crispy_forms',
     
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
