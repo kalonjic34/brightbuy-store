0a12fbe48c76e2e3f454c9dd86d661bc1a80b33e
Christian <kalonjic34@gmail.com>
2025-09-02 15:12:48 +0200

    Implement user login, dashboard, and logout in account

--- ecommerce/account/urls.py
+++ ecommerce/account/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('email-verification-sent', views.email_verification_sent, name='email-verification-sent'),
     path('email-verification-success', views.email_verification_success, name='email-verification-success'),
     path('email-verification-failed', views.email_verification_failed, name='email-verification-failed'),
+    path('my-login', views.my_login, name='my-login'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('user-logout', views.user_logout, name='user-logout'),
     
 ]
