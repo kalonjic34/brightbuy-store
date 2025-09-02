dbec143ddc3d2513dcc82b36a8f2d8524e401a17
Christian <kalonjic34@gmail.com>
2025-09-02 16:50:51 +0200

    Implement UpdateUserForm with email validation

--- ecommerce/account/forms.py
+++ ecommerce/account/forms.py
@@ -28,3 +28,17 @@ class CreateUserForm(UserCreationForm):
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget = TextInput())
     password = forms.CharField(widget = PasswordInput())
+    
+    
+class UpdateUserForm(forms.ModelForm):
+    password = None
+    
+    def __init__(self, *args, **kwargs):
+        super(UpdateUserForm, self).__init__(*args, **kwargs)
+        
+        self.fields['email'].required = True
+    
+    class Meta:
+        model = User
+        fields = ['username','email']
+        exclude = ['password1','password1']
