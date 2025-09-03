4fc48597fedaf984bf3064d15f87d3103bcce753
Christian <kalonjic34@gmail.com>
2025-09-03 15:52:47 +0200

    Implement email validation in UpdateUserForm

--- ecommerce/account/forms.py
+++ ecommerce/account/forms.py
@@ -33,12 +33,25 @@ class LoginForm(AuthenticationForm):
 class UpdateUserForm(forms.ModelForm):
     password = None
     
+    class Meta:
+        model = User
+        fields = ['username','email']
+        exclude = ['password1','password1']
+    
     def __init__(self, *args, **kwargs):
         super(UpdateUserForm, self).__init__(*args, **kwargs)
         
         self.fields['email'].required = True
     
-    class Meta:
-        model = User
-        fields = ['username','email']
-        exclude = ['password1','password1']
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        
+        if User.objects.filter(email=email).exclude(pk=self.instance.pk).exists():
+            raise forms.ValidationError('This email is invalid')
+        
+        if len(email) >= 350:
+            
+            raise forms.ValidationError('Your email is too long')
+        
+        return email
+    
