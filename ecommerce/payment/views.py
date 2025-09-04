faf1d50025a685e4a75547d313d361680593cf49
Christian <kalonjic34@gmail.com>
2025-09-04 17:32:21 +0200

    Implement checkout view

--- ecommerce/payment/views.py
+++ ecommerce/payment/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+def checkout(request):
+    
+    return render(request, 'payment/checkout.html')
 def payment_success(request):
     
     return render(request, 'payment/payment-success.html')
