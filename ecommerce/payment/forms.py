4874bec20d345453b0d01a09ca950cdf2eefb5e5
Christian <kalonjic34@gmail.com>
2025-09-04 17:31:20 +0200

    Refactor ShippingForm to use Django native forms

--- ecommerce/payment/forms.py
+++ ecommerce/payment/forms.py
@@ -1,7 +1,7 @@
-from django import froms
+from django import forms
 from . models import ShippingAddress
 
-class ShippingForm(Forms.ModelForm):
+class ShippingForm(forms.ModelForm):
     class Meta:
         model = ShippingAddress
         fields = ['full_name', 'email', 'address1','address2', 'city','state','zipcode']
