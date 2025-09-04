4961fc8ff23581180534fc6531ae1f12e18a60d9
Christian <kalonjic34@gmail.com>
2025-09-04 16:21:51 +0200

    Implement ShippingAddress model

--- ecommerce/payment/models.py
+++ ecommerce/payment/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class ShippingAddress(models.Model):
+    full_name = models.CharField(max_length=300)
+    email = models.EmailField(max_length=250)
+    address1 = models.CharField(max_length=300)
+    address2 = models.CharField(max_length=300)
+    city = models.CharField(max_length=250)
+    state = models.CharField(max_length=250, null=True, blank=True)
+    zipcode = models.CharField(max_length=250, null=True, blank=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE, null=True, blank=True)
+    
+    class Meta:
+        verbose_name_plural ='ShippingAddress'
+        
+    def __str__(self):
+        return 'Shipping Address - '+ str(self.id)
+    
